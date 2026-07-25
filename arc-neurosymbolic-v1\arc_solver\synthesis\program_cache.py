999fe9e2acb7d0023c1cc4e861467e67e9811cc6
najmulhossainnj <165473683+najmulhossainnj@users.noreply.github.com>
2026-07-25 22:18:11 +0600

    Add arc-neurosymbolic-v1\arc_solver\synthesis\program_cache.py

---
+++ arc-neurosymbolic-v1\arc_solver\synthesis\program_cache.py
@@ -0,0 +1,6 @@
+class ProgramCache:
+    def __init__(self):
+        self.cache={}
+    def get(self,key): return self.cache.get(key)
+    def put(self,key,value): self.cache[key]=value
+    def clear(self): self.cache.clear()
