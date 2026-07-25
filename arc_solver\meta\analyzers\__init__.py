d8a0315d7ab135f08d91a7518de8656eca0c76c3
najmulhossainnj <165473683+najmulhossainnj@users.noreply.github.com>
2026-07-25 22:33:00 +0600

    Add arc_solver\meta\analyzers\__init__.py

---
+++ arc_solver\meta\analyzers\__init__.py
@@ -0,0 +1,36 @@
+from .base import Analyzer, ProgramCandidate
+from .color_substitution import ColorSubstitutionAnalyzer
+from .translation import TranslationAnalyzer
+from .replication import ArrowReplicateAnalyzer
+from .misc_analyzers import (
+    SymmetryCompleteAnalyzer,
+    GravityFallAnalyzer,
+    BorderCropAnalyzer,
+    PatternExtensionAnalyzer,
+    BlockCycleAnalyzer,
+    ParallelogramAlignAnalyzer,
+    DiagonalChainAnalyzer,
+)
+
+ALL_ANALYZERS = [
+    ColorSubstitutionAnalyzer(),
+    TranslationAnalyzer(),
+    SymmetryCompleteAnalyzer(),
+    GravityFallAnalyzer(),
+    PatternExtensionAnalyzer(),
+    BlockCycleAnalyzer(),
+    ArrowReplicateAnalyzer(),
+    ParallelogramAlignAnalyzer(),
+    DiagonalChainAnalyzer(),
+    BorderCropAnalyzer(),
+]
+
+ALL_ANALYZERS.sort(key=lambda a: a.priority)
+
+__all__ = [
+    "Analyzer", "ProgramCandidate", "ALL_ANALYZERS",
+    "ColorSubstitutionAnalyzer", "TranslationAnalyzer", "ArrowReplicateAnalyzer",
+    "SymmetryCompleteAnalyzer", "GravityFallAnalyzer", "BorderCropAnalyzer",
+    "PatternExtensionAnalyzer", "BlockCycleAnalyzer",
+    "ParallelogramAlignAnalyzer", "DiagonalChainAnalyzer",
+]
