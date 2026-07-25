03666c39512efb6d1bc1e273d5e70eaf7bea2b08
najmulhossainnj <165473683+najmulhossainnj@users.noreply.github.com>
2026-07-25 22:44:46 +0600

    Remove invalid backslash path arc_solver\synthesis\program_cache.py

--- arc_solver\synthesis\program_cache.py
+++
@@ -1,6 +0,0 @@
-class ProgramCache:
-    def __init__(self):
-        self.cache={}
-    def get(self,key): return self.cache.get(key)
-    def put(self,key,value): self.cache[key]=value
-    def clear(self): self.cache.clear()
